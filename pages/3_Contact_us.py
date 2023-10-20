bd12a471252a5c571af13f1e0a3d06b618befd1b
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-20 11:01:15 +0800

    Update 3_Contact_us.py

--- pages/3_Contact_us.py
+++ pages/3_Contact_us.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.write("<span style='font-size: 18px;color: Black;'>这是一个刚刚开始搭建的Web app,可实现的功能很简陋，希望您能对各方面提出宝贵意见：</span>", unsafe_allow_html=True)
-st.write("<span style='font-size: 15px;color: #191970;'>1.希望加入的功能</span>", unsafe_allow_html=True)
-st.write("<span style='font-size: 15px;color: #191970;'>2.页面布局的任何调整</span>", unsafe_allow_html=True)
-st.write("<span style='font-size: 15px;color: #191970;'>3.现有Bug的优化</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 20px;color: Black;'>这是一个刚刚开始搭建的Web app,因此页面和功能都很简陋，因此更需要您的宝贵意见：</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 18px;color: #191970;'>1.希望加入的功能</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 18px;color: #191970;'>2.页面布局的任何调整</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 18px;color: #191970;'>3.现有Bug的优化</span>", unsafe_allow_html=True)
+
