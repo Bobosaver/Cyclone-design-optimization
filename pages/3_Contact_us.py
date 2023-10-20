d719c14825226debb7bb73bfe04df0cc736341da
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-20 11:07:47 +0800

    Update 3_Contact_us.py

--- pages/3_Contact_us.py
+++ pages/3_Contact_us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import matplotlib.image as mpimg
 
 st.write("<span style='font-size: 20px;color: Black;'>这是一个刚刚开始搭建的Web app,因此页面和功能都很简陋，因此更需要您的宝贵意见：</span>", unsafe_allow_html=True)
 st.write("<span style='font-size: 18px;color: #191970;'>1.希望加入的功能</span>", unsafe_allow_html=True)
