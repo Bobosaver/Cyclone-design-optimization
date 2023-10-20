fb06b940de5a3085fa5102672a44a3ce60b3117e
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-20 10:29:44 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.image as mpimg
 
 #放置合肥水泥院LOGO
-logo = img2 = mpimg.imread(r'Sinoma_ico.png')
+logo = mpimg.imread(r'Sinoma_ico.png')
 st.image(logo,caption='')
 
     
