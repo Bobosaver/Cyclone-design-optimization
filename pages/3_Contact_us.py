a94e44ef0facb45d65ff1fb848a046e310faff75
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-20 11:04:31 +0800

    Update 3_Contact_us.py

--- pages/3_Contact_us.py
+++ pages/3_Contact_us.py
@@ -5,7 +5,7 @@ st.write("<span style='font-size: 18px;color: #191970;'>1.希望加入的功能<
 st.write("<span style='font-size: 18px;color: #191970;'>2.页面布局的任何调整</span>", unsafe_allow_html=True)
 st.write("<span style='font-size: 18px;color: #191970;'>3.现有Bug的优化</span>", unsafe_allow_html=True)
 img = mpimg.imread(r'./pages/images/OK.jpg')
-st.image(img,caption='',)
+st.image(img,caption='十分感谢',)
 
 
 
