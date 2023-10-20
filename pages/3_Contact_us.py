d98cc34148ff40c1116116850b759cd855d4b4ec
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-20 11:22:31 +0800

    Update 3_Contact_us.py

--- pages/3_Contact_us.py
+++ pages/3_Contact_us.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import matplotlib.image as mpimg
 
-st.write("<span style='font-size: 20px;color: Black;'>这是一个刚刚开始搭建的Web app,因此页面和功能都很简陋，因此更需要您的宝贵意见：</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 20px;color: Black;'>这是一个刚刚开始搭建的Web app，页面和功能都很简陋，因此更需要您的宝贵意见：</span>", unsafe_allow_html=True)
 st.write("<span style='font-size: 18px;color: #191970;'>1.希望加入的功能</span>", unsafe_allow_html=True)
 st.write("<span style='font-size: 18px;color: #191970;'>2.页面布局的任何调整</span>", unsafe_allow_html=True)
 st.write("<span style='font-size: 18px;color: #191970;'>3.现有Bug的优化</span>", unsafe_allow_html=True)
 
 img = mpimg.imread(r'./pages/images/OK.jpg')
 st.image(img,caption='')
-
+st.write("<span style='font-size: 18px;color: #191970;'>如果您有任何意见或是使用的困扰，不妨致电或者是微信联系我。</span>", unsafe_allow_html=True)
+st.write("<span style='font-size: 20px;color: green;'>微信：</span>","<span style='font-size: 20px;color: green;'>ytf2810192</span>",unsafe_allow_html=True)
 
 
