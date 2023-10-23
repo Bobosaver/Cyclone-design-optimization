b430f62d9b67c83396b97a1725f4184ff2e585c1
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 09:26:18 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,13 +6,9 @@ logo = mpimg.imread(r'Sinoma_ico.png',width=30)
 st.image(logo,caption='')
 
 #合肥水泥院简介
-.shadowbox {
-  width: 15em;
-  border: 1px solid #333;
-  box-shadow: 8px 8px 5px #444;
-  padding: 8px 12px;
-  background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);
-}
+st.write("<h3 style='color: black; text-indent:2em'>合肥水泥研究设计院有限公司是世界财富500强企业——中国建材集\
+         团的全资子公司，是中国建材行业重点科研院所和甲级设计单位。合肥院主要从事以水泥为特色的无机非金属材料领域技\
+         术和装备的研发、生产和销售，以及提供配套技术服务，在水泥生产技术的研发应用、装备制造和技术服务领域享有盛誉。</h3>",unsafe_allow_html=True)
 
 
 
