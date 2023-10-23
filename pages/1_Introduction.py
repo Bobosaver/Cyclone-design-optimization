a9e25d0f0029804853e218b2e685273bdc51a7de
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 11:43:12 +0800

    Update 1_Introduction.py

--- pages/1_Introduction.py
+++ pages/1_Introduction.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import matplotlib.image as mpimg
 
-st.write("<h1 style='color: black; text-align:center;'>旋风分离器拟合响应面项目介绍</h3>",unsafe_allow_html=True)
+st.title('旋风分离器拟合响应面项目介绍')
 st.write("<h3 style='color: black; text-indent:2em'>这个Web App针对旋风筒结构关键参数及主要边界条件进行参数化仿真，并对结果进行后处理，以选定最优参数</h3>",unsafe_allow_html=True)
 st.write("<h3 style='color: black;'>目前具有以下功能：</h3>", unsafe_allow_html=True)
 st.write("<h5 style='color: red;'>1.响应面拟合(风速/粒径—收集率）</h5>", unsafe_allow_html=True)
