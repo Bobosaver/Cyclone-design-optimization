1886053f91e6fd630c6ed5899337351bb1948db7
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 15:07:13 +0800

    Update 3_Help.py

--- pages/3_Help.py
+++ pages/3_Help.py
@@ -3,7 +3,7 @@ import matplotlib.image as mpimg
 
 st.title('帮助')
 #选择参考文档
-op_func =['***旋风分离器响应面拟合***','**旋风分离器收集率曲线拟合及收集效率计算**','***旋风分离器总收集效率计算***']
+op_func =['***旋风分离器响应面拟合***','***旋风分离器收集率曲线拟合及收集效率计算***','***旋风分离器总收集效率计算***']
 choose_func = st.radio('您对以下哪个功能有所疑问？',op_func,index=None)
 
 if choose_func == op_func[0]:
