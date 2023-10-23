1d3759a5274f8871410d3f86d9f0e1ee2660199b
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 09:50:32 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,16 +5,17 @@ import matplotlib.image as mpimg
 logo = mpimg.imread(r'Sinoma_ico.png')
 st.image(logo,caption='')
 
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+
 #合肥水泥院简介
-st.write("<h5 style='color: red; text-indent:2em'>合肥水泥研究设计院有限公司是世界财富500强企业——中国建材集\
+st.write("<h5 style='color: red; text-indent:2em; line-height:2px; '>合肥水泥研究设计院有限公司是世界财富500强企业——中国建材集\
          团的全资子公司，是中国建材行业重点科研院所和甲级设计单位。合肥院主要从事以水泥为特色的无机非金属材料领域技\
          术和装备的研发、生产和销售，以及提供配套技术服务，在水泥生产技术的研发应用、装备制造和技术服务领域享有盛誉。</h5>",unsafe_allow_html=True)
 
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
 st.write("")
 st.write("")
 st.write("")
