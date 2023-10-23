655a4c2cf3d8b0372d1096daef6786d11e999da7
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 10:58:06 +0800

    Update 2_Existing function.py

--- pages/2_Existing function.py
+++ pages/2_Existing function.py
@@ -221,7 +221,6 @@ elif choose_func == op_func[1]:
     collection_curve()
     
 elif choose_func == op_func[2]:
-    a,b = collection_curve()
     m = st.sidebar.slider("请输入粒径(μm):",1,30,30)
     shoujilv = 1-np.exp(-(m/collection_curve()[0])**collection_curve()[1])
     st.sidebar.write("<span style='color: red;'>收集率为：</span>", unsafe_allow_html=True)
