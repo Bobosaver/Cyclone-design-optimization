bafabfd306321d995cc68e14e8984e670f7b2ad6
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 15:10:43 +0800

    Update 3_Help.py

--- pages/3_Help.py
+++ pages/3_Help.py
@@ -21,8 +21,11 @@ if choose_func == op_func[0]:
             img = mpimg.imread(r'./Help/三次样条插值.png')
             st.image(img,caption='')
         if choose_algorithm == op_algorithm[2]:
-            st.write("<p style='color: red; text-indent:2em'>当前主要的响应面拟合算法包括多项式插值算法和基于大\
-            数据样本的机器学习算法。本Web app可选择采用三种拟合算法：</p>",unsafe_allow_html=True)            
+            st.write('')
+            st.write('')
+            st.write("<p style='color: red; text-indent:2em'>梯度提升算法的"提升"指将多个简单模型合并到单个复合模型中的一种方式。\
+            这也是为什么增强被称为累加模型的原因，因为简单模型（也称为弱学习者）一次添加一个，同时保持模型中的现有树不变。随着我们组\
+            合越来越多的简单模型，完整的最终模型成为一个更强大的预测变量。</p>",unsafe_allow_html=True)            
         if choose_algorithm == op_algorithm[3]:
             
         
