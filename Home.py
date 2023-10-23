e35f8e441f60c597b8057ca0cbbd851a3988df68
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 08:50:15 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,8 @@ import matplotlib.image as mpimg
 logo = mpimg.imread(r'Sinoma_ico.png')
 st.image(logo,caption='')
 
+#一些装备的介绍
+col1, col2, col3 = st.columns(3)
 with col1:
     img1 = mpimg.imread(r'./pages/images/合肥水泥院介绍.jpg')
     st.image(img1,caption='')
