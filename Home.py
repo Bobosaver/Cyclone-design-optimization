3e5d8f9a6fa39b2c716e1b2aba7f5a41e5dab22b
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-10-23 09:47:13 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,6 +25,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     img1 = mpimg.imread(r'./pages/images/合肥水泥院介绍.jpg')
     st.image(img1,caption='')
+    st.write("<h5 style='color: red; text-indent:2em'>公司平台</h5>",unsafe_allow_html=True)
     st.write("<spna style='color: black; text-indent:2em'>创新和运营“双元驱动”，“四位一体”生态链，\
     实现科研——工程——科研互相反哺的良性循环；拥有多个国家、省市级科研平台,多项技术成果被列入国家重大科技\
     成果推广计划。2020年被评为国家技术创新示范企业，2022年在国资委“双百行动”专项考核中被评为标杆企业。</span>",unsafe_allow_html=True)
@@ -33,10 +34,12 @@ with col1:
 with col2:
     img2 = mpimg.imread(r'./pages/images/HFGG系列辊压机.jpg')
     st.image(img2,caption='')
+    st.write("<h5 style='color: red; text-indent:2em'>HFGG系列高性能辊压机</h5>",unsafe_allow_html=True)
 
 with col3:
     img3 = mpimg.imread(r'./pages/images/HP强涡流型多风道煤粉燃烧器.png')
-    st.image(img3,caption='')  
+    st.image(img3,caption='') 
+    st.write("<h5 style='color: red; text-indent:2em'>HP强涡流型多风道煤粉燃烧器</h5>",unsafe_allow_html=True)
 
 
     
