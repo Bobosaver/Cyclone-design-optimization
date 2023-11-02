73ac05fc0566cdc3b1b139575b623676e9b7d7d1
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-11-02 11:11:43 +0800

    Update 1_👉Introduction.py

--- pages/1_👉Introduction.py
+++ pages/1_👉Introduction.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.image as mpimg
 from streamlit_extras.app_logo import add_logo
 
-add_logo(r'C:\Users\Yin\Desktop\仿真结果记录\旋风分离器\Cyclone-design-optimization-main\合肥水泥院logo.png',height=60)
+add_logo(r'合肥水泥院logo.png',height=60)
 st.write("<p style='color: black; text-align:center;font-weight:bold;font-size:35px'>旋风分离器拟合响应面项目介绍</p>",unsafe_allow_html=True)
 st.write("<p style='color: black; text-indent:2em;font-weight:bold;font-size:22px;'>这个Web App针对旋风筒结构关键参数\
 及主要边界条件进行参数化仿真，并对结果进行后处理，以选定最优参数</p>",unsafe_allow_html=True)
