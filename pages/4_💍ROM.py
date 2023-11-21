3c3bd9da25aade1d75d7758a42bbb2b4f34c32f6
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-11-21 15:52:02 +0800

    Update 4_💍ROM.py

--- pages/4_💍ROM.py
+++ pages/4_💍ROM.py
@@ -5,4 +5,4 @@ import time
 st.write("<p style='color: black; text-align:center;font-weight:bold;font-size:35px'>旋风分离器ROM降阶模型</p>",unsafe_allow_html=True)
 
 if st.button('点击进行'+':orange[可交互式ROM降阶模型]',use_container_width=True):
-    st.components.v1.iframe(src="http://4166e44h40.imdo.co:31205/",width=700,height=500,scrolling=False)
+   st.write('<iframe src="https://30days.streamlit.app/?embed=true",height="450",width="700"></iframe>',unsafe_allow_html=True)
