91fea79b85d779ea2ca0a9438a2ef1968edc4534
Bobosaver <148194306+Bobosaver@users.noreply.github.com>
2023-11-21 16:09:54 +0800

    Update 4_💍ROM.py

--- pages/4_💍ROM.py
+++ pages/4_💍ROM.py
@@ -2,4 +2,9 @@ import streamlit as st
 import matplotlib.image as mpimg 
 import time
 st.write("<p style='color: black; text-align:center;font-weight:bold;font-size:35px'>旋风分离器ROM降阶模型</p>",unsafe_allow_html=True)
-st.link_button('点击进行'+':orange[可交互式ROM降阶模型]','http://4166e44h40.imdo.co',use_container_width=True)
+st.link_button('点击进行'+':orange[可交互式ROM降阶模型]','https://4166e44h40.imdo.co',use_container_width=True)
+st.write("<p style='color: black; text-align:center;font-weight:bold;font-size:35px'>旋风分离器ROM降阶模型</p>",unsafe_allow_html=True)
+
+
+if st.button('点击进行'+':orange[可交互式ROM降阶模型]',use_container_width=True):
+    st.components.v1.iframe(src="https://4166e44h40.imdo.co",width=700,height=500,scrolling=False)
